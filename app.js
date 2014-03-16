58c8445f66e00909a6898ae8bd6fa38332d72e20
Paul Brzeski <mail@paulbrzeski.com>
2014-03-16 16:40:08 +0800

    Merging in new changes

---
+++ app.js
@@ -0,0 +1,31 @@
+/*
+
+	LANGENIUM
+	Main Application
+
+*/
+
+
+/*
+	Modules
+*/
+
+
+var 	modules = require('./modules')();
+
+// Initialize models
+modules.import_classes(modules, modules.models, './models');
+
+// Initialize controllers
+modules.import_classes(modules, modules.controllers, './controllers');
+
+// Initialize routes
+modules.import_classes(modules, modules.routes, './routes');
+
+// Start server
+modules.server.listen(process.env['HTTP_PORT']); // dev
+
+// FOR DEBUGGING ONLY
+if (process.env['HOST_URL'].indexOf('dev') >= 0) {
+	global.modules = modules;
+}

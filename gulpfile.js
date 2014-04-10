08ce203b7296d5dffa79850bdf5243aacfa92f8a
Paul Brzeski <mail@paulbrzeski.com>
2014-04-10 23:16:04 +0800

    Got 3rd party libs working!
    Onto getting the programmatic running of Gulp... I really don't want to
    run multiple tasks on dev :P

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,18 @@ var browserify = require('gulp-browserify');
 // Basic usage
 gulp.task('scripts', function() {
     // Single entry point to browserify
-    gulp.src('browserify-bundles/client-libs.js')
+    gulp.src(['browserify-bundles/client-libs.js'])
         .pipe(browserify({
-        	transform: ['browserify-shim']
+        	shim: {
+        		jquery: {
+        			path: './node_modules/jquery/dist/jquery.js',
+        			exports: '$'
+        		},
+        		three: {
+        			path: './node_modules/three/three.js',
+        			exports: 'THREE'
+        		}
+        	}
         }))
         .pipe(gulp.dest('./public/js'))
 });

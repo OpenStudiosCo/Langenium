0daa161b193d9a37e73ce81b1b4fcdf866bdeea7
Paul Brzeski <mail@paulbrzeski.com>
2014-04-28 01:56:21 +0800

    - Trying SSL again

--- public/scripts/L.js
+++ public/scripts/L.js
@@ -2,7 +2,7 @@ var L = function() {
 	var L = {
 		name: "Langenium client application",
 		gui: new dat.GUI(),
-		socket: io.connect()
+		socket: io.connect(window.location.hostname + ':443')
 	};
 
 	console.log("Client application has started")

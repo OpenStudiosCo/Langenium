e42a067f68b9f3a59200df791b78d4f6caa2f545
Paul Brzeski <mail@paulbrzeski.com>
2016-12-11 18:12:00 +0800

    Setup IA

---
+++ new/dist/wiki/index.js
@@ -0,0 +1,6 @@
+function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {var pug_debug_sources = {"pages\u002Fwiki\u002Findex.pug":"h1 Wiki \r\n"};
+var pug_indent = [];
+;pug_debug_line = 1;pug_debug_filename = "pages\u002Fwiki\u002Findex.pug";
+pug_html = pug_html + "\n\u003Ch1\u003E";
+;pug_debug_line = 1;pug_debug_filename = "pages\u002Fwiki\u002Findex.pug";
+pug_html = pug_html + "Wiki \u003C\u002Fh1\u003E";} catch (err) {pug_rethrow(err, pug_debug_filename, pug_debug_line, pug_debug_sources[pug_debug_filename]);};return pug_html;}

b03f3a175f0f0222387a6023404e734120efb6b6
Paul Brzeski <mail@paulbrzeski.com>
2016-12-13 23:09:54 +0800

    Removed rolled up vendor config and files. Switched to old way. I guess rollup should be used per project hehe

--- rollup.vendor.config.js
+++
@@ -1,9 +0,0 @@
-import uglify from 'rollup-plugin-uglify';
-
-export default {
-  context: 'window',
-  format: 'iife',
-  //plugins: [ uglify() ],
-  entry: 'src/vendor.js',
-  dest: 'dist/vendor.js'
-};

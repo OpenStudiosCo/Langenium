b386e62a2406bedce5a9b7a6925dbca219af5ac0
Paul Brzeski <mail@paulbrzeski.com>
2023-12-22 05:35:07 +0000

    v6 - Adding kettlefish dev script

---
+++ client/dev.js
@@ -0,0 +1,22 @@
+// Simple build script for development.
+const chokidar = require('chokidar');
+const spawn = require('cross-spawn');
+
+function run (script) {
+  spawn('npm', ['run', script], { stdio: 'inherit' });
+}
+
+run('dev:11ty');
+run('build:esbuild');
+
+chokidar.watch(['src', '../game'], { }).on('all', (event, path) => {
+  if (event === 'change') {
+    console.log(event, path);
+    if (path.endsWith('.js') || path.endsWith('.ts')) {
+        run('build:esbuild');
+    }
+  }
+});
+
+// // Load the server application
+// require('child_process').fork('./src/server/server.js');

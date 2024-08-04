41f5cf00c16d3fd2ebb2c9ac6665c0f4b6261293
Paul Brzeski <mail@paulbrzeski.com>
2024-08-04 08:47:31 +0000

    #32 - Adding minimum node versions and lock, scaffolding bot and adding pirate model files

---
+++ client/src/app/scenograph/scenes/assets/bot.js
@@ -0,0 +1,17 @@
+/**
+ * Enemy bot.
+ * 
+ * Currently hardcoded to use the Pirate aircraft.
+ */
+import * as THREE from 'three';
+
+/**
+ * Internal libs and helpers.
+ */
+import l from '@/helpers/l.js';
+import { brightenMaterial, proceduralMetalMaterial } from '@/scenograph/materials.js';
+import Valiant from '#/game/src/objects/aircraft/valiant.js';
+
+export default class Bot {
+
+}

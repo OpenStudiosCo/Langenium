847493d5164751725b431cf4f9a97a75db3a1c46
Paul Brzeski <mail@paulbrzeski.com>
2024-08-30 10:01:06 +0000

    #32 - Adding heads up display class scaffold

---
+++ client/src/app/scenograph/overlays/heads-up-display.js
@@ -0,0 +1,42 @@
+/***
+ * @name            Heads Up Display (HUD)
+ * @description     Draws aircraft HUD
+ * @namespace       l.scenograph.overlays.hud
+ * @memberof        l.scenograph.overlays
+ * @global
+ */
+
+/**
+ * Internal libs and helpers.
+ */
+import l from '@/helpers/l.js';
+
+/**
+ * Vendor libs and base class.
+ */
+import * as THREE from "three";
+
+export default class HeadsUpDisplay {
+
+
+    constructor() {
+
+    
+    }
+
+    /**
+     * Animate hook.
+     * 
+     * This method is called within the main animation loop and
+     * therefore must only reference global objects or properties.
+     * 
+     * @method animate
+     * @memberof Scanners
+     * @global
+     * @note All references within this method should be globally accessible.
+    **/
+    animate() {
+        
+    }
+
+}

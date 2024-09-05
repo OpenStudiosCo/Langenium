19f0c548dac300a06a04b89bf90503ffa9e5c688
Paul Brzeski <mail@paulbrzeski.com>
2024-09-05 08:52:08 +0000

    #32 - Adding new icons and targetting scanner class scaffold

---
+++ client/src/app/ui/targetting_scanner.js
@@ -0,0 +1,41 @@
+/***
+ * @name            Targetting Scanner
+ * @description     Target list and management screen
+ * @namespace       l.ui.targetting_scanner
+ * @memberof        l.ui
+ * @global
+ */
+
+/**
+ * Internal libs and helpers.
+ */
+import l from '@/helpers/l.js';
+
+/**
+ * Vendor libs and base class.
+ */
+import * as THREE from "three";
+
+export default class TargettingScanner {
+
+    constructor() {
+
+
+    }
+
+    /**
+     * Animate hook.
+     * 
+     * This method is called within the main animation loop and
+     * therefore must only reference global objects or properties.
+     * 
+     * @method animate
+     * @memberof Scanners
+     * @global
+     * @note All references within this method should be globally accessible.
+    **/
+    animate() {
+
+    }
+
+}

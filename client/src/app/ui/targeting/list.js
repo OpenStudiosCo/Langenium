d0670f58bab9672e585035ff23ede8dd11b161c4
Paul Brzeski <mail@paulbrzeski.com>
2024-09-10 17:48:42 +0000

    #32 - Making sensors list table sortable

--- client/src/app/ui/targeting/list.js
+++ client/src/app/ui/targeting/list.js
@@ -54,12 +54,11 @@ export default class List {
             'refinery': 'Union Refinery',
         }
 
-
         l.current_scene.scene.traverse( mesh => {
-            
+
             // Check if targetable and not the current player.
             let targetable = mesh.userData && mesh.userData.targetable ? true : false;
-            if ( targetable && mesh.uuid != l.current_scene.objects.player.mesh.uuid) {
+            if ( targetable && mesh.uuid != l.current_scene.objects.player.mesh.uuid ) {
                 let item = JSON.parse( JSON.stringify( l.ui.targeting.list.item_template ) );
 
                 let icon_class = '';
@@ -81,22 +80,77 @@ export default class List {
         return html;
     }
 
+    sortTable( n ) {
+        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        table = document.querySelector( '#ui-targeting-list table' );
+        switching = true;
+        // Set the sorting direction to ascending:
+        dir = "asc";
+        /* Make a loop that will continue until
+        no switching has been done: */
+        while ( switching ) {
+            // Start by saying: no switching is done:
+            switching = false;
+            rows = table.rows;
+            /* Loop through all table rows (except the
+            first, which contains table headers): */
+            for ( i = 1; i < ( rows.length - 1 ); i++ ) {
+                // Start by saying there should be no switching:
+                shouldSwitch = false;
+                /* Get the two elements you want to compare,
+                one from current row and one from the next: */
+                x = rows[ i ].getElementsByTagName( "TD" )[ n ];
+                y = rows[ i + 1 ].getElementsByTagName( "TD" )[ n ];
+                /* Check if the two rows should switch place,
+                based on the direction, asc or desc: */
+                if ( dir == "asc" ) {
+                    if ( x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase() ) {
+                        // If so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                } else if ( dir == "desc" ) {
+                    if ( x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase() ) {
+                        // If so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if ( shouldSwitch ) {
+                /* If a switch has been marked, make the switch
+                and mark that a switch has been done: */
+                rows[ i ].parentNode.insertBefore( rows[ i + 1 ], rows[ i ] );
+                switching = true;
+                // Each time a switch is done, increase this count by 1:
+                switchcount++;
+            } else {
+                /* If no switching has been done AND the direction is "asc",
+                set the direction to "desc" and run the while loop again. */
+                if ( switchcount == 0 && dir == "asc" ) {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
+    }
+
     updateTable() {
         let rows = document.querySelectorAll( l.ui.targeting.list.containerId + ' tr' );
         rows.forEach( targetIcon => {
-            let targetObject = l.current_scene.scene.getObjectByProperty( 'uuid', targetIcon.dataset.uuid);
+            let targetObject = l.current_scene.scene.getObjectByProperty( 'uuid', targetIcon.dataset.uuid );
 
             // Update the distance to target.
             let distance = targetObject.position.distanceTo( l.current_scene.objects.player.mesh.position );
             if ( distance > 1000 ) {
-                distance = Math.round(Math.round(distance) / 10) / 100;
-                targetIcon.querySelector('.distance').innerHTML = distance + 'km';
+                distance = Math.round( Math.round( distance ) / 10 ) / 100;
+                targetIcon.querySelector( '.distance' ).innerHTML = distance + 'km';
             }
             else {
-                distance = Math.round(distance);
-                targetIcon.querySelector('.distance').innerHTML = distance + 'm';
+                distance = Math.round( distance );
+                targetIcon.querySelector( '.distance' ).innerHTML = distance + 'm';
             }
-        } );    
+        } );
     }
 
 
@@ -114,7 +168,7 @@ export default class List {
     update() {
 
         // Check if we need to rebuild the target lock list HTML.
-        if ( l.ui.targeting.list.needsUpdate ) {  
+        if ( l.ui.targeting.list.needsUpdate ) {
 
             // Populate the current target lock icons HTML.
             l.ui.targeting.list.container.innerHTML = l.ui.targeting.list.getTable();

7f0f8d41c8fa354fbb3711935eab40ba58615e6f
Paul Brzeski <mail@paulbrzeski.com>
2024-10-19 07:25:08 +0000

    #32 - Switching to CSS based styles for targets

--- client/src/app/scenograph/overlays/scanners.js
+++ client/src/app/scenograph/overlays/scanners.js
@@ -31,21 +31,19 @@ export default class Scanners {
 
         this.trackedObjects.push({
             mesh: l.current_scene.objects.bot.mesh,
-            symbol: this.getSymbolElement( 'diamond' ),
+            domElement: this.getSymbolElement( 'diamond' )
         });
 
         l.current_scene.objects.cargo_ships.forEach( async ( cargo_ship, i ) => {
             this.trackedObjects.push({
                 mesh: cargo_ship,
-                symbol: this.getSymbolElement( 'diamond' ),
+                domElement: this.getSymbolElement( 'diamond' )
             });
           } );
 
         this.trackedObjects.forEach( trackedObject => {
-            this.container.appendChild( trackedObject.symbol );
+            this.container.appendChild( trackedObject.domElement );
         } );
-
-        //l.ui.targeting.locked.needsUpdate = true;
 
     }
 
@@ -65,15 +63,98 @@ export default class Scanners {
      * 
      * @param {*} THREE.Object3D currently in the scene
      * @returns [x,y] screen space coordinates
+     * @global
+     * @note All references within this method should be globally accessible.
      */
-    getSymbolPosition ( object ) {
+    animateSymbolPosition ( object, frustum ) {
         const vector = new THREE.Vector3();
         object.getWorldPosition(vector);
         vector.project(l.scenograph.cameras.active);
 
-        const x = (vector.x * 0.5 + 0.5) * l.scenograph.width;
-        const y = (1 - (vector.y * 0.5 + 0.5)) * l.scenograph.height;
+        let x = (vector.x * 0.5 + 0.5) * l.scenograph.width;
+        let y = (1 - (vector.y * 0.5 + 0.5)) * l.scenograph.height;
+
+        if ( x < l.scenograph.overlays.scanners.offset ) {
+            x = l.scenograph.overlays.scanners.offset;
+        }
+
+        if ( y < l.scenograph.overlays.scanners.offset ) {
+            y = l.scenograph.overlays.scanners.offset;
+        }
+
+        if ( x > l.scenograph.width - l.scenograph.overlays.scanners.offset ) {
+            x = l.scenograph.width - l.scenograph.overlays.scanners.offset;
+        }
+
+        if ( y > l.scenograph.height - l.scenograph.overlays.scanners.offset ) {
+            y = l.scenograph.height - l.scenograph.overlays.scanners.offset;
+        }
+
+        if (!frustum.containsPoint(object.position)) {
+
+            let diffX = (l.scenograph.width / 2) - x ;
+            let diffZ = (l.scenograph.height / 2) - y ;
+
+            if (Math.abs(diffX) > Math.abs(diffZ)) {
+                if ( diffX < 0 ) {
+                    x = l.scenograph.width - l.scenograph.overlays.scanners.offset;
+                }
+                else {
+                    x = l.scenograph.overlays.scanners.offset;
+                }
+            }
+            else {
+                if ( diffZ < 0 ) {
+                    y = l.scenograph.height - l.scenograph.overlays.scanners.offset;
+                }
+                else {
+                    y = l.scenograph.overlays.scanners.offset;
+                }
+            }
+
+        }
+
         return [ x, y ];
+    }
+
+    /**
+     * Animates the tracked object.
+     * 
+     * @param {*} trackedObject 
+     * @param {*} frustum 
+     */
+    animateTrackedObject( trackedObject, frustum ) {
+        let [ x, y ] = l.scenograph.overlays.scanners.animateSymbolPosition(trackedObject.mesh, frustum);
+
+        // Check if the object is within the scanner area.
+        if ( l.scenograph.overlays.scanners.getTargetLock( x, y ) ) {
+            trackedObject.domElement.classList.add('active');
+        }
+        else {
+            trackedObject.domElement.classList.remove('active');
+        }
+
+        trackedObject.domElement.style.left = `${x-5}px`;
+        trackedObject.domElement.style.top = `${y-5}px`;
+    }
+
+    // @todo: Move scanner logic to aircraft equipment update code.
+    getTargetLock( x, y ) {
+        if ( 
+            x > l.scenograph.width * 0.375 &&
+            x < l.scenograph.width * 0.625 &&
+            y > l.scenograph.height * 0.375 &&
+            y < l.scenograph.height * 0.625
+        ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    animateTarget() {
+
     }
 
     /**
@@ -94,74 +175,7 @@ export default class Scanners {
         
         l.scenograph.cameras.active.updateProjectionMatrix();
         l.scenograph.overlays.scanners.trackedObjects.forEach(trackedObject => {
-            let symbol = trackedObject.symbol.querySelector('.symbol');
-            let targetingEffect = trackedObject.symbol.querySelector('.targeting-effect');
-            let [ x, y ] = l.scenograph.overlays.scanners.getSymbolPosition(trackedObject.mesh);
-
-            if ( x < l.scenograph.overlays.scanners.offset ) {
-                x = l.scenograph.overlays.scanners.offset;
-            }
-
-            if ( y < l.scenograph.overlays.scanners.offset ) {
-                y = l.scenograph.overlays.scanners.offset;
-            }
-
-            if ( x > l.scenograph.width - l.scenograph.overlays.scanners.offset ) {
-                x = l.scenograph.width - l.scenograph.overlays.scanners.offset;
-            }
-
-            if ( y > l.scenograph.height - l.scenograph.overlays.scanners.offset ) {
-                y = l.scenograph.height - l.scenograph.overlays.scanners.offset;
-            }
-
-            if (!frustum.containsPoint(trackedObject.mesh.position)) {
-
-                let diffX = (l.scenograph.width / 2) - x ;
-                let diffZ = (l.scenograph.height / 2) - y ;
-
-                if (Math.abs(diffX) > Math.abs(diffZ)) {
-                    if ( diffX < 0 ) {
-                        x = l.scenograph.width - l.scenograph.overlays.scanners.offset;
-                    }
-                    else {
-                        x = l.scenograph.overlays.scanners.offset;
-                    }
-                }
-                else {
-                    if ( diffZ < 0 ) {
-                        y = l.scenograph.height - l.scenograph.overlays.scanners.offset;
-                    }
-                    else {
-                        y = l.scenograph.overlays.scanners.offset;
-                    }
-                }
-
-            }
-            //else {
-                
-            //}
-
-            // Check if the object is within the scanner area.
-            // @todo: Move scanner logic to aircraft equipment update code.
-            if ( 
-                x > l.scenograph.width * 0.375 &&
-                x < l.scenograph.width * 0.625 &&
-                y > l.scenograph.height * 0.375 &&
-                y < l.scenograph.height * 0.625
-            ) {
-                symbol.style.background = 'rgba(200, 0, 200, 0.5)';
-                symbol.style.border = 'solid 2px rgba(200, 0, 200, 1)';
-                targetingEffect.style.display = 'block';
-            }
-            else {
-                symbol.style.background = 'none';
-                symbol.style.border = 'solid 1px rgba(0, 0, 200, 1)';
-                targetingEffect.style.display = 'none';
-            }
-
-            trackedObject.symbol.style.left = `${x-5}px`;
-            trackedObject.symbol.style.top = `${y-5}px`;
-
+            l.scenograph.overlays.scanners.animateTrackedObject(trackedObject, frustum);
         });
     }
 

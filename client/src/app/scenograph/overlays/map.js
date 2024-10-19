d0f066d4646382273f31e3b740e47585dcfe975b
Paul Brzeski <mail@paulbrzeski.com>
2024-10-19 17:21:12 +0000

    #32 - Adding player fov to mini map

--- client/src/app/scenograph/overlays/map.js
+++ client/src/app/scenograph/overlays/map.js
@@ -22,12 +22,16 @@ export default class Map {
 
         this.container = document.querySelector('#game_overlay #map');
 
-        this.icons = document.querySelector('#templates #map_icons');
+        // Visibility range in meters.
+        this.zoom = 1000;
 
-        // // Create the vertical lines
-        // this.lineElement = document.createElement('div');
-        // this.lineElement.id = 'hud-lines';
-        // this.container.appendChild(this.lineElement);
+        // Reusable icon SVGs
+        this.icons = this.getIconSVGs();
+
+        // Create the fov icon
+        this.fov = document.createElement('div');
+        this.fov.id = 'map-fov';
+        this.container.appendChild(this.fov);
 
         // // Create the label to track air speed.
         // this.aspdElement = this.createLabel( 'aspd', 'AIRSPEED: 0km/h');
@@ -47,10 +51,37 @@ export default class Map {
 
     }
 
+    getIconSVGs() {
+        let icons = {};
+        let iconTemplate = document.createElement('div');
+        iconTemplate.innerHTML = document.querySelector('#templates #map_icons').innerHTML;
+
+        icons.aircraft = iconTemplate.querySelector('.aircraft').innerHTML;
+        icons.ship = iconTemplate.querySelector('.ship').innerHTML;
+        icons.special = iconTemplate.querySelector('.special').innerHTML;
+        icons.structure = iconTemplate.querySelector('.structure').innerHTML;
+
+        return icons;
+    }
+
+    /**
+     * Adds and removes icons from the map based on proximity and zoom level.
+     */
+    updateMapIcons() {
+
+    }
+
+    /**
+     * Animates the mini map position of symbol
+     */
+    animateMapIcons() {
+
+    }
+
     /**
      * Animate hook.
      * 
-     * This method is called within the main animation loop and
+     * This method is called within the main animation loop andw
      * therefore must only reference global objects or properties.
      * 
      * @method animate
@@ -59,7 +90,14 @@ export default class Map {
      * @note All references within this method should be globally accessible.
     **/
     animate() {
+        let heading = THREE.MathUtils.radToDeg( l.current_scene.objects.player.state.rotation.y );
+        heading = heading % 360;
+        if (heading < 0) {
+            heading += 360;
+        }
+        heading = Math.round(360 - heading);
 
+        l.scenograph.overlays.map.container.style.transform = 'rotate(' + heading + 'deg)';
     }
 
 }

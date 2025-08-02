78538db3951c17f64a7f5d5882ace5bd5c194988
Paul Brzeski <mail@paulbrzeski.com>
2025-08-02 14:23:41 +0000

    #31 - Tweaking hangar appearance

--- client/src/app/scenograph/objects/structures/hangar.js
+++ client/src/app/scenograph/objects/structures/hangar.js
@@ -24,18 +24,18 @@ export default class Hangar {
 
     constructor() {
         this.ready = false;
-        this.size = 150;
+        this.size = 5;
 
     }
 
     async load() {
 
-         //const material = new THREE.MeshBasicMaterial( {color: 0xff0000, transparent: true, opacity: 1.0, side: THREE.DoubleSide} ); 
+        const clearMaterial = new THREE.MeshBasicMaterial( {color: 0xffFFFF, transparent: true, visible: false, side: THREE.DoubleSide} ); 
     
         const material = proceduralBuilding( {
             uniforms: {
                 time        :  { value: 0.0 },
-                scale       :  { value: .005 },                                         // Scale
+                scale       :  { value: .05 },                                         // Scale
                 lacunarity  :  { value: 2.0 },                                          // Lacunarity
                 randomness  :  { value: 1.0 },                                          // Randomness
                 emitColour1 :  { value: new THREE.Vector4( 0.0, 0.0, 0.0, 0.25 ) },     // Emission gradient colour 1
@@ -51,38 +51,49 @@ export default class Hangar {
         window.extractor = {};
         window.extractor.outer = material;
 
-        let outerMesh = new Brush( new THREE.CylinderGeometry( 8, 8, 100, 8, 1, false ), material );
+        let outerMesh = new Brush( new THREE.BoxGeometry( 10, 5, 10, 2, 2, 2 ), material );
         outerMesh.scale.setScalar( this.size );
         outerMesh.updateMatrixWorld();
 
-        // let innerMesh = new Brush( new THREE.CylinderGeometry( 7.5, 7.5, 99, 8, 1, false ), material );
-        // innerMesh.position.y = 1000;
-        // innerMesh.scale.setScalar( this.size );
-        // innerMesh.updateMatrixWorld();
+        let innerMesh = new Brush( new THREE.BoxGeometry( 9, 4, 9, 2, 2, 2 ), material );
+        innerMesh.position.z = -10;
+        innerMesh.scale.setScalar( this.size );
+        innerMesh.updateMatrixWorld();
 
-        // let result = new THREE.Mesh(
-        //     new THREE.BufferGeometry(),
-        //     new THREE.MeshBasicMaterial()
-        // );
+        let result = new THREE.Mesh(
+            new THREE.BufferGeometry(),
+            new THREE.MeshBasicMaterial()
+        );
 
-        // // Constructive Solid Geometry (csg) Evaluator.
-        // let csgEvaluator;
-        // csgEvaluator = new Evaluator();
-        // csgEvaluator.useGroups = true;
-        // csgEvaluator.evaluate( outerMesh, innerMesh, HOLLOW_SUBTRACTION, result );
-        // result.name = 'outer';
+        // Constructive Solid Geometry (csg) Evaluator.
+        let csgEvaluator;
+        csgEvaluator = new Evaluator();
+        csgEvaluator.useGroups = true;
+        csgEvaluator.evaluate( outerMesh, innerMesh, HOLLOW_SUBTRACTION, result );
+        result.name = 'outer';
 
-        // let innerMesh2 = new THREE.Mesh( new THREE.CylinderGeometry( 7.5, 7.5, 100, 8, 1, true ), material.clone() );
-        // window.extractor.inner = innerMesh2.material;
+        let innerGeo = new THREE.BoxGeometry( 9, 4, 9, 2, 2, 2 );
+        let innerMeshMaterial = material.clone();
+        innerMeshMaterial.uniforms.scale.value = 0.25;
+
+        let innerMesh2 = new THREE.Mesh( innerGeo, [
+            innerMeshMaterial,
+            innerMeshMaterial,
+            innerMeshMaterial,
+            innerMeshMaterial,
+            innerMeshMaterial,
+            clearMaterial.clone()
+        ] );
+        window.extractor.inner = innerMesh2.material;
         // innerMesh2.material.uniforms.scale.value = 0.8;
-        // innerMesh2.position.y = 0;
-        // innerMesh2.scale.setScalar( this.size );
-        // innerMesh2.updateMatrixWorld();
-        // innerMesh2.name = 'inner';
+        innerMesh2.position.y = 0;
+        innerMesh2.scale.setScalar( this.size );
+        innerMesh2.updateMatrixWorld();
+        innerMesh2.name = 'inner';
 
         this.mesh = new THREE.Object3D();
-        this.mesh.add( outerMesh );
-        // this.mesh.add( innerMesh2 );
+        this.mesh.add( result );
+        this.mesh.add( innerMesh2 );
         this.mesh.userData.targetable = true;
         this.mesh.userData.objectClass = 'hangar';
 
--- client/src/app/scenograph/scenes/overworld.js
+++ client/src/app/scenograph/scenes/overworld.js
@@ -177,7 +177,7 @@ export default class Overworld extends SceneBase {
     // @todo: #31 Implement first person mode and a way to go between being in the hangar and being in the aircraft
     l.current_scene.objects.hangar = new Hangar();
     await l.current_scene.objects.hangar.load();
-    // l.current_scene.objects.hangar.mesh.position.z = 0;
+    l.current_scene.objects.hangar.mesh.position.y = 15;
     // l.current_scene.objects.hangar.mesh.position.x = 0;
     l.current_scene.scene.add(
       l.current_scene.objects.hangar.mesh

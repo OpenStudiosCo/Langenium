5a53344c46e4e77e2892c0437089f1fea063aeeb
Paul Brzeski <mail@paulbrzeski.com>
2025-08-03 09:28:46 +0000

    #31 - Refactoring hangar class and tidying up cruft

--- client/src/app/scenograph/objects/structures/hangar.js
+++ client/src/app/scenograph/objects/structures/hangar.js
@@ -20,6 +20,8 @@ export default class Hangar {
     // @todo: Dynamic hangars with different dimensions
     hangarSize;
 
+    materials;
+
     // Dimensions of player living quarters
     // @todo: Dynamic hangars which don't all have one.
     quartersSize;
@@ -32,7 +34,16 @@ export default class Hangar {
 
     constructor() {
 
-        this.hangarSize = { 
+        this.materials = {};
+
+        // Based on office door dimensions.
+        this.corridorSize = {
+            width: 8.2,
+            height: 20.4,
+            depth: 20,
+        }
+
+        this.hangarSize = {
             width: 10,
             height: 5,
             depth: 10
@@ -51,97 +62,122 @@ export default class Hangar {
 
     async load() {
 
-        const clearMaterial = new THREE.MeshBasicMaterial( {color: 0xffFFFF, transparent: true, visible: false, side: THREE.DoubleSide} ); 
-    
-        const material = proceduralBuilding( {
+        /**
+         * Setup Hangar materials
+         */
+
+        this.materials.clear = new THREE.MeshBasicMaterial( { color: 0xffFFFF, transparent: true, visible: false, side: THREE.DoubleSide } );
+        this.materials.metal = proceduralBuilding( {
             uniforms: {
-                time        :  { value: 0.0 },
-                scale       :  { value: .05 },                                         // Scale
-                lacunarity  :  { value: 2.0 },                                          // Lacunarity
-                randomness  :  { value: 1.0 },                                          // Randomness
-                emitColour1 :  { value: new THREE.Vector4( 0.0, 0.0, 0.0, 0.25 ) },     // Emission gradient colour 1
-                emitColour2 :  { value: new THREE.Vector4( 0.158, 1., 1., .9 ) },       // Emission gradient colour 2
-                shadowFactor:  { value: 0.03 },
-                shadowOffset:  { value: 0.1 },
+                time: { value: 0.0 },
+                scale: { value: .05 },                                         // Scale
+                lacunarity: { value: 2.0 },                                          // Lacunarity
+                randomness: { value: 1.0 },                                          // Randomness
+                emitColour1: { value: new THREE.Vector4( 0.0, 0.0, 0.0, 0.25 ) },     // Emission gradient colour 1
+                emitColour2: { value: new THREE.Vector4( 0.158, 1., 1., .9 ) },       // Emission gradient colour 2
+                shadowFactor: { value: 0.03 },
+                shadowOffset: { value: 0.1 },
             }
         } );
+        this.materials.metal.transparent = true;
+        this.materials.metal.side = THREE.DoubleSide;
+        // Clone of the material to customize scaling.
+        this.materials.hangar = this.materials.metal.clone();
+        this.materials.hangar.uniforms.scale.value = 0.25;
 
-        material.transparent = true;
-        material.side = THREE.DoubleSide;
+        let hangarMesh = await this.loadHangarMesh();
 
-        window.extractor = {};
-        window.extractor.outer = material;
+        
+        /**
+         * Corridor mesh
+         */
+        let corridorMesh = await this.loadCorridorMesh();
 
-        let innerMeshMaterial = material.clone();
-        innerMeshMaterial.uniforms.scale.value = 0.25;
-
-
-        let outerMesh = new Brush( new THREE.BoxGeometry( this.hangarSize.width, this.hangarSize.height, this.hangarSize.depth, 2, 2, 2 ), [
-            innerMeshMaterial,
-            innerMeshMaterial,
-            innerMeshMaterial,
-            innerMeshMaterial,
-            innerMeshMaterial,
-            clearMaterial.clone()
-        ]  );
-        outerMesh.scale.setScalar( this.size );
-        outerMesh.updateMatrixWorld();
-
-        // Create door geometry
-        var doorWidth = 8.2;
-        var doorHeight = 20.4;
-        var doorDepth = 20;
-        var doorGeometry = new THREE.BoxGeometry( doorWidth, doorHeight, doorDepth );
-
-        let innerMesh = new Brush( doorGeometry, material );
-        innerMesh.position.x = - (this.hangarSize.width * this.size) / 2 ;
-        innerMesh.position.y = ( - (this.hangarSize.height * this.size) / 2 ) + doorHeight * 0.25;
-        innerMesh.rotation.y = Math.PI / 2;
-        innerMesh.scale.setScalar( this.size / 10 );
-        innerMesh.updateMatrixWorld();
-
-        window.hangarDoor = innerMesh;
-
-        let result = new THREE.Mesh(
-            new THREE.BufferGeometry(),
-            new THREE.MeshBasicMaterial()
-        );
+        // Setup the result mesh.
+        let result = new THREE.Mesh( new THREE.BufferGeometry(), new THREE.MeshBasicMaterial() );
 
         // Constructive Solid Geometry (csg) Evaluator.
         let csgEvaluator;
         csgEvaluator = new Evaluator();
         csgEvaluator.useGroups = true;
-        csgEvaluator.evaluate( innerMesh, outerMesh, ADDITION, result );
+        csgEvaluator.evaluate( corridorMesh, hangarMesh, ADDITION, result );
         result.name = 'outer';
 
         let innerGeo = new THREE.BoxGeometry( this.quartersSize.width, this.quartersSize.height, this.quartersSize.depth, 2, 2, 2 );
 
-        let innerMesh2 = new Brush( innerGeo, material );
-        window.quarters = innerMesh2;
-        innerMesh2.material.uniforms.scale.value = 0.8;
+        /**
+         * Player quarters mesh.
+         */
+        let quartersMesh = new Brush( innerGeo, this.materials.metal );
+        window.quarters = quartersMesh;
+        quartersMesh.material.uniforms.scale.value = 0.8;
         // Offset player quarters by the hangars half width
-        innerMesh2.position.x = (- (this.hangarSize.width * this.size) / 2);
+        quartersMesh.position.x = ( - ( this.hangarSize.width * this.size ) / 2 );
         // Offset player quarters by the access corridor
-        innerMesh2.position.x += - doorDepth * 0.25;
+        quartersMesh.position.x += - this.corridorSize.depth * 0.25;
         // Offset player quarters by half the quarters width
-        innerMesh2.position.x += (- (this.quartersSize.width * this.size) / 2);
+        quartersMesh.position.x += ( - ( this.quartersSize.width * this.size ) / 2 );
         // Offset for intersection
-        innerMesh2.position.x += 1;
+        quartersMesh.position.x += 1;
 
-        innerMesh2.position.y = ( - (this.quartersSize.height * this.size) / 2 );
+        quartersMesh.position.y = ( - ( this.quartersSize.height * this.size ) / 2 );
 
-        innerMesh2.scale.setScalar( this.size );
-        innerMesh2.updateMatrixWorld();
-        innerMesh2.name = 'inner';
+        quartersMesh.scale.setScalar( this.size );
+        quartersMesh.updateMatrixWorld();
+        quartersMesh.name = 'inner';
 
-        csgEvaluator.evaluate( result, innerMesh2, ADDITION, result );
+        // @todo: #31 - Implement hierarchical operations
+        // csgEvaluator.evaluate( result, innerMesh2, ADDITION, result );
 
         this.mesh = new THREE.Object3D();
         this.mesh.add( result );
         // this.mesh.add( innerMesh2 );
-        this.mesh.userData.targetable = true;
+        this.mesh.userData.targetable = false;
         this.mesh.userData.objectClass = 'hangar';
 
+    }
+
+    /**
+     * Creates the Hangar's main mesh.
+     */
+    async loadHangarMesh() {
+        /**
+         * Hangar mesh
+         */
+        let hangarMesh = new Brush( new THREE.BoxGeometry( this.hangarSize.width, this.hangarSize.height, this.hangarSize.depth, 2, 2, 2 ), [
+            this.materials.hangar,
+            this.materials.hangar,
+            this.materials.hangar,
+            this.materials.hangar,
+            this.materials.hangar,
+            this.materials.clear.clone()
+        ] );
+
+        hangarMesh.scale.setScalar( this.size );
+
+        hangarMesh.updateMatrixWorld();
+
+        return hangarMesh;
+    }
+
+    /**
+     * Creates the corridor mesh.
+     */
+    async loadCorridorMesh() {
+        
+        var doorGeometry = new THREE.BoxGeometry( this.corridorSize.width, this.corridorSize.height, this.corridorSize.depth );
+
+        let corridorMesh = new Brush( doorGeometry, this.materials.metal );
+
+        corridorMesh.position.x = - ( this.hangarSize.width * this.size ) / 2;
+        corridorMesh.position.y = ( - ( this.hangarSize.height * this.size ) / 2 ) + this.corridorSize.height * 0.25;
+        corridorMesh.rotation.y = Math.PI / 2;
+
+        corridorMesh.scale.setScalar( this.size / 10 );
+
+        corridorMesh.updateMatrixWorld();
+
+        return corridorMesh;
     }
 
     /**
